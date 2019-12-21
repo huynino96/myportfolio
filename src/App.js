04e22064072ea8144d50ebfa48b3f9cdbda5a37e
Huy Ho <tuanhuy0496@yahoo.com>
2019-12-21 08:57:07 +0700

    Finishing Navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from './component/main';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,22 +11,23 @@ function App() {
     <Layout>
         <Header title="Title" scroll>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/project">Project</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+            <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/project">Project</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
             <div className="page-content" />
+            <Main/>
         </Content>
     </Layout>
 </div>
--- src/component/main.js
+++ src/component/main.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
-import { Switch, Link } from 'react-router-dom'
+import { Switch, Link, Route } from 'react-router-dom';
 
-class Main extends Component{
-    render(){
-        return(
-            
-            <Switch>
-                <Link path='/aboutme'>Home Page</Link>/>
-            </Switch>
-        )
-    }
-}
+import Landing from './landingpage';
+import About from './aboutme';
+import Contact from './contact';
+import Project from './project';
+import Resume from './resume';
+
+
+const Main = () => (
+    <Switch> 
+        <Route exact path="/" component={Landing}/>
+        <Route path="/aboutme" component={About}/>
+        <Route exact path="/contact" component={Contact}/>
+        <Route exact path="/project" component={Project}/>
+        <Route exact path="/resume" component={Resume}/>
+    </Switch>
+        
+
+)
 
 export default Main;
